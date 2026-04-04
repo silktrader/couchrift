9d092bb5d748c829909f45b9eee9fdf6a41b4231
silktrader <silktrader@gmail.com>
2026-04-04 21:55:14 +0200

    feat(web): add `uploadAvatar()` to userService

--- apps/web/src/lib/userService.svelte.ts
+++ apps/web/src/lib/userService.svelte.ts
@@ -1,6 +1,7 @@
 import type { User } from 'better-auth'
 import { createContext } from 'svelte'
 import { authClient } from '$lib/auth-client'
+import { apiUpload } from '$lib/apiFetch'
 
 type SessionStore = ReturnType<typeof authClient.useSession>
 
@@ -14,6 +15,19 @@ export class UserService {
       this.user = session.data.user
     })
 
+  }
+
+  async uploadAvatar(file: File) {
+    const formData = new FormData()
+    formData.append('avatar', file)
+
+    const result = await apiUpload<{ fileName: string }>(`users/me/avatar`, formData)
+
+    if (result.type === 'success') {
+      this.user = { ...this.user, image: result.data.fileName }
+    }
+
+    return result
   }
 }
 
