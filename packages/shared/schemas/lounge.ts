e8aaf58f05b9f43b0f83beb0bd7dce8f715cc1ec
silktrader <silktrader@gmail.com>
2026-04-08 09:13:13 +0200

    refactor: compose lounge schemas with `LoungeSettingsSchema`

--- packages/shared/schemas/lounge.ts
+++ packages/shared/schemas/lounge.ts
@@ -1,10 +1,12 @@
 import { Type, type Static } from '@sinclair/typebox'
 import { Timestamp, NullableTimestamp, NanoId12, NanoId8, Shortcode, LoungeParticipant } from './primitives.ts'
 
+export const LoungeSettingsSchema = Type.Object({
+  maxDuration: Type.Integer({ minimum: 20 })
+})
+
 export const LoungeCreateSchema = Type.Object({
-  settings: Type.Object({
-    maxDuration: Type.Integer({ minimum: 20 })
-  })
+  settings: LoungeSettingsSchema
 })
 
 export const LoungeResponseSchema = Type.Object({
@@ -14,10 +16,9 @@ export const LoungeResponseSchema = Type.Object({
   endedAt:      NullableTimestamp,
   startedAt:    NullableTimestamp,
   shortcode:    Shortcode,
-  settings:     Type.Object({
-    maxDuration: Type.Integer({ minimum: 20 })
-  }),
+  settings:     LoungeSettingsSchema,
   participants: Type.Array(LoungeParticipant)
 })
-
 export type LoungeResponse = Static<typeof LoungeResponseSchema>
+
+export type LoungeSettings = Static<typeof LoungeSettingsSchema>
