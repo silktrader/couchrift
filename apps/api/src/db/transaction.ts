4bd099d03972eee60ce1b1e299b7709d89bdae7b
silktrader <silktrader@gmail.com>
2026-04-22 12:16:41 +0200

    feat(api): add transaction helper that handles returned result types

---
+++ apps/api/src/db/transaction.ts
@@ -0,0 +1,33 @@
+import type { Success, Failure } from '@couchrift/shared/utilities/result.ts'
+import db from './index.ts'
+
+class TransactionRollback<T> extends Error {
+  constructor(public readonly result: T) {
+    super('Rollback')
+  }
+}
+
+/**
+ * Wraps the provided function within an `IMMEDIATE` transaction that detects `Failure` return types
+ * and throws exceptions for rollbacks.
+ */
+export function runTransactionWithRollback<E extends string, T extends Success>(
+  fn: () => T | Failure<E>): T | Failure<E> {
+  const tx = db.transaction(() => {
+    const result = fn()
+
+    // Trigger a rollback when the function returns a failure object
+    if (!result.ok) throw new TransactionRollback(result)
+    return result
+  })
+
+  try {
+    // Ensure the write lock is acquired
+    return tx.immediate()
+  } catch (e) {
+    if (e instanceof TransactionRollback)
+      return e.result
+
+    throw e // re-throw to bubble up unexpected DB errors
+  }
+}
