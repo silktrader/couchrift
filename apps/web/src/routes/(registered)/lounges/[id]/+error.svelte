f224d35f9629d08bc065d47236afe96323c2c7aa
silktrader <silktrader@gmail.com>
2026-04-27 20:04:54 +0200

    feat(web): add completed lounge error page

---
+++ apps/web/src/routes/(registered)/lounges/[id]/+error.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+  import { page } from '$app/state'
+  import { browser } from '$app/environment'
+  import { ChevronRight, ArrowLeft } from '@lucide/svelte'
+  import { Button } from '$lib/components/ui/button'
+  import * as Item from '$lib/components/ui/item/index.js'
+
+  function goBack() {
+    if (browser) window.history.back()
+  }
+
+  let errorMessage = $derived.by(() => {
+    switch (page?.error?.type) {
+      case 'LOUNGE_MISSING':
+        return 'No lounge found'
+      case 'validation':
+        return 'Invalid lounge ID'
+      case 'FORBIDDEN_ACCESS':
+        return 'You do not have access to this lounge'
+      default:
+        return 'Unknown error'
+    }
+  })
+</script>
+
+<div class="flex flex-1 flex-col items-center mb-12">
+
+  <header class="w-full h-12 mb-6">
+    <Button size="icon-lg" variant="ghost" onclick={goBack}>
+      <ArrowLeft class="size-8"/>
+    </Button>
+  </header>
+
+  <div class="flex flex-col gap-2 items-center">
+    <img src="/couch_error.webp"
+         alt="Error"
+         class="object-contain z-10 w-3/4 mb-12"/>
+    <h1 class="text-muted-foreground">An error occurred while fetching lounge data:</h1>
+    <h1 class="font-semibold text-2xl">{errorMessage}</h1>
+  </div>
+
+  <!-- Empty -->
+  <div class="flex-1"></div>
+
+  <div class="flex flex-col gap-6 justify-end">
+
+    <Item.Root variant="outline">
+      {#snippet child({props})}
+        <a href="/home" {...props}>
+          <Item.Content>
+            <Item.Title>Completed Lounges</Item.Title>
+            <Item.Description>
+              List the lounges in which you participated.
+            </Item.Description>
+          </Item.Content>
+          <Item.Actions>
+            <ChevronRight class="size-4"/>
+          </Item.Actions>
+        </a>
+      {/snippet}
+    </Item.Root>
+
+    <Item.Root variant="outline">
+      {#snippet child({props})}
+        <a href="/home" {...props}>
+          <Item.Content>
+            <Item.Title>Go Home</Item.Title>
+            <Item.Description>
+              View ongoing lounges or start a new one.
+            </Item.Description>
+          </Item.Content>
+          <Item.Actions>
+            <ChevronRight class="size-4"/>
+          </Item.Actions>
+        </a>
+      {/snippet}
+    </Item.Root>
+  </div>
+
+</div>
